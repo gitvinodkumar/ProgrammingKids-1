8c6b0b81759601bffcc2363e38eb5dab37b24e5b
user <user@user-PC>
2018-08-25 20:10:20 -0700

    this is our destiny

--- StudentProjectWithnumber/src/com/nt/Student.java
+++ StudentProjectWithnumber/src/com/nt/Student.java
@@ -5,7 +5,8 @@ public class Student {
 	public static void main(String[] args) {
 		
 		System.out.println("hii helooo how are u");
-		////////////////////////////////
+	
+		  System.out.println("hii helooo this is not destiny thhis is our future");
 
 	}
 
