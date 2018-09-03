77cbacedd74ffc1b307fc3eaaea54c346d5d0d04
user <user@user-PC>
2018-09-03 00:07:32 -0700

    hsfjksjkfhsjkdfhsjkhfj

---
+++ CyclicDependentInjection/src/main/java/com/nt/beans/Student.java
@@ -0,0 +1,28 @@
+package com.nt.beans;
+
+public class Student {
+	
+	
+	private String name;
+	private String add;
+	
+	private Course course;
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAdd(String add) {
+		this.add = add;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", add=" + add + ", course=" + course + "]";
+	}
+	
+	
+	
+	
+
+}
